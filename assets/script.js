df0daecf7ff9002630597fdf2e94361153dc2ea8
spb71 <shem.baijoo@gmail.com>
2021-09-07 15:10:40 -0400

    Changed up source path

--- assets/script.js
+++ assets/script.js
@@ -30,23 +30,16 @@ $(document).ready(function () {
             textAreaEl.addClass("future");
         }
     });
-    
-    $("#hour9 .description").val(localStorage.getItem("hour9"));
-    $("#hour10 .description").val(localStorage.getItem("hour10"));
-    $("#hour11 .description").val(localStorage.getItem("hour11"));
-    $("#hour12 .description").val(localStorage.getItem("hour12"));
-    $("#hour13 .description").val(localStorage.getItem("hour13"));
-    $("#hour14 .description").val(localStorage.getItem("hour14"));
-    $("#hour15 .description").val(localStorage.getItem("hour15"));
-    $("#hour16 .description").val(localStorage.getItem("hour16"));
-    $("#hour17 .description").val(localStorage.getItem("hour17"));
 
     saveBtnEl.on("click", function(event) {
         event.preventDefault();
 
-        var text = saveBtnEl.siblings(".description").val();
-        var time = saveBtnEl.parent().attr("id");
-
-        localStorage.setItem(time, text);
+        for (var i = 0; i < 9; i++) {
+            localStorage.setItem("hour" + i, $("#note" + i + "input").val());
+        }
     });
+
+    for (var i = 1; i < 11; i++) {
+        $("#note" + i + "input").val(localStorage.getItem("note" + i));
+    }
 });
