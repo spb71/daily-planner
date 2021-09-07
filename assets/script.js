e7dc79b2bac75ab873c8a60d6641dfc87d438473
spb71 <shem.baijoo@gmail.com>
2021-09-07 16:20:49 -0400

    Added parameters in JS

--- assets/script.js
+++ assets/script.js
@@ -10,36 +10,33 @@ $(document).ready(function () {
         timeDisplayEl.text(today.format("dddd, MMMM Do, [at] hh:mm:ss a"));
     }
     setInterval(displayTime, 1000);
-    var currentHour = moment().hour();
+    var currentHour = moment().hours();
 
-    textAreaEl.each(function () {
-        var id = textAreaEl.attr("id");
-        var timeBlockHour = moment().hours(id, "HH");
+    textAreaEl.each(function (index, element) {
+        var timeBlockHour = index + 9;
+        var textarea = element;
+        console.log(textarea);
         console.log(timeBlockHour);
-        if (timeBlockHour < currentHour) {
-            textAreaEl.addClass("past");
-            textAreaEl.removeClass("future");
-            textAreaEl.removeClass("present");
-        } else if (timeBlockHour === currentHour) {
-            textAreaEl.removeClass("past");
-            textAreaEl.removeClass("future");
-            textAreaEl.addClass("present");
-        } else {
-            textAreaEl.removeClass("past");
-            textAreaEl.removeClass("present");
-            textAreaEl.addClass("future");
+        for (var i = textAreaEl[0]; i < textAreaEl.length; i++) {
+            if (timeBlockHour < currentHour) {
+                textAreaEl.addClass("past");
+                textAreaEl.removeClass("future");
+                textAreaEl.removeClass("present");
+            } else if (timeBlockHour === currentHour) {
+                textAreaEl.removeClass("past");
+                textAreaEl.removeClass("future");
+                textAreaEl.addClass("present");
+            } else {
+                textAreaEl.removeClass("past");
+                textAreaEl.removeClass("present");
+                textAreaEl.addClass("future");
+            }
         }
     });
 
     saveBtnEl.on("click", function(event) {
         event.preventDefault();
 
-        for (var i = 0; i < 9; i++) {
-            localStorage.setItem("hour" + i, $("#note" + i + "input").val());
-        }
+        //function for localStorage
     });
-
-    for (var i = 1; i < 11; i++) {
-        $("#note" + i + "input").val(localStorage.getItem("note" + i));
-    }
 });
